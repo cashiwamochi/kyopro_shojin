20f213b8c1aa2474a29c2d2128a2156a654a0ae3
cashiwamochi <ryo.cv.0311@gmail.com>
2020-06-01 00:16:34 +0900

    atcoder,abc,169,solved,a,b,c

---
+++ atcoder/abc/169/b/main.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	unsigned long long ans = 1;
+	const unsigned long long limit = 1e18;
+	std::vector<unsigned long long> inputs;
+	for (int i = 0; i < n; ++i) {
+		unsigned long long tmp;
+		scanf("%llu", &tmp);
+		if (tmp == 0) {
+			printf("0\n");
+			return 0;
+		}
+		inputs.push_back(tmp);
+	}
+	std::sort(inputs.begin(), inputs.end());
+	for (int i = 0; i < n; ++i) {
+		ans *= inputs[i];
+		if (ans > limit/inputs[i]) {
+			printf("-1\n");
+			return 0;
+		}
+		else 
+			ans *= inputs[i];
+	}
+
+	printf("%llu\n", ans);
+
+	return 0;
+}
---
+++ atcoder/abc/169/d/main.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int main() {
+	long long a;
+	double b;
+	std::cin >> a >> b;
+
+	long long ans = a * b;
+
+	std::cout << ans << std::endl;
+
+	return 0;
+}
