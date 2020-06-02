e7a90981a3d2feb4446b82829991a449f221bfc9
cashiwamochi <ryo.cv.0311@gmail.com>
2020-06-03 02:07:21 +0900

    add template cpp for atcoder

---
+++ atcoder/template_main.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+        return 0;
+}
